164b75bd49d6d243ab55db4887e72cf3df26f73e
ahmed.zubairu <bazubairu@st.ug.edu.gh>
2024-07-06 11:54:06 +0000

    error deployement

---
+++ src/main/java/org/sorting/config/WebConfig.java
@@ -0,0 +1,31 @@
+<<<<<<< Updated upstream
+package org.sorting.config;public class WebConfig {
+=======
+package org.sorting.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "org.sorting")
+public class WebConfig {
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+>>>>>>> Stashed changes
+}
